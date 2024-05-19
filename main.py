fc8030bb9ec0a26fbda775cc10818be0d9105849
ElenaNovikova1978 <languagebridge@mail.ru>
2024-05-19 14:15:18 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+print ('Сейчас на Земле появился новый вид роботов')
+name= 'радар'
+print (name [::-1])
+name ='норма'
+print (name [::-1])
+print ('кенг '+ 'уру')
+print ('уру'+'кенг')
+name='нейропрограммирование'
+print (name[0],name[2],name[4],name[6],name[8],name[10],name[12],name[14],name[16],name[18],name[20])
+print (name[1],name[3],name[5],name[7],name[9],name [11],name[13],name[15],name[17],name[19])
+name='нейропластичность'
+print (name[15],name[14],name[13],name[12],name[11],name[10],name[9],name[8],name[7],name[6],name[5],name[4],name[3],name[2],name[1])
